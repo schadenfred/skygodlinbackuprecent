bfdcfb9ec71c27609f0940c8d4d7d106c488860c
Fred Schoeneman <fred.schoeneman@gmail.com>
2010-12-08 18:31:53 -0800

    up to speed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,17 @@
 module ApplicationHelper
 
-  # Return a title on a per-page basis.
   def title
     base_title = "Skygodlin"
     if @title.nil?
       base_title
     else
-      "#{base_title} | #{@title}"
+      "#{base_title} | #{@title} "
     end
   end
+
+  def logo
+    logo = image_tag("logo.png", :alt => "Skygodlin", :class => "round")
+  end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Skygodlin::Application.routes.draw do
-  get "pages/home"
+  get "users/new"
 
-  get "pages/contact"
+  match '/signup',        :to => 'users#new'
 
-  get "pages/about"
+  match '/contact',       :to => 'pages#contact'
+  match '/about',         :to => 'pages#about'
+  match '/help',          :to => 'pages#help'
+  match '/terms',         :to => 'pages#terms'
+  match '/privacy',       :to => 'pages#privacy'
 
-  get "pages/terms"
-
-  get "pages/help"
-
-  get "pages/privacy"
-
+  root :to => "pages#home"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -68,3 +67,4 @@ Skygodlin::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 end
+
